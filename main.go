863efa7844f80737a9860880b6d06cbec17dcf6b
Joseph Osborne <jd.osborne5@icloud.com>
2021-10-31 20:17:01 +0000

    setting out basic interface and struct for the fields

--- main.go
+++ main.go
@@ -2,7 +2,27 @@ package main
 
 import "fmt"
 
+type Field struct {
+	vals [][]int
+	desc string
+}
+
+type Field_interface interface {
+	call_desc()
+	print_vals()
+}
+
+func (f Field) call_desc() {
+	fmt.Println(f.desc)
+}
+
+func (f Field) print_vals() {
+	fmt.Println(f.vals)
+}
+
 func main() {
 
-	fmt.Println("Hello World!")
+	var test_f Field_interface = Field{vals: [][]int{{1, 2, 3, 4}, {4, 3, 2, 1}}, desc: "a testing field"}
+	test_f.print_vals()
+	test_f.call_desc()
 }
